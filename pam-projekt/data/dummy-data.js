687e5e2f3413d6f0501dbcf06a0fe8b1d4343746
kacper <wotstyle@onet.pl>
2023-03-22 15:56:56 +0100

    Data preparation

---
+++ pam-projekt/data/dummy-data.js
@@ -0,0 +1,326 @@
+import Country from "../models/countries";
+
+export const COUNTRIES = [
+    new Country(
+        'c1',
+        'Albania',
+        3038594,
+        28748,
+        'Tirana'
+    ),
+    new Country(
+        'c2',
+        'Andora',
+        78000,
+        468,
+        'Andora'
+    ),
+    new Country(
+        'c3',
+        'Austria',
+        8000000,
+        83854,
+        'Wiedeń'
+    ),
+    new Country(
+        'c4',
+        'Belgia',
+        10000000,
+        30528,
+        'Bruksela'
+    ),
+    new Country(
+        'c5',
+        'Białoruś',
+        9413446,
+        207600,
+        'Mińsk'
+    ),
+    new Country(
+        'c6',
+        'Bośnia i Hercegowina',
+        3791662,
+        41000,
+        'Sarajewo'
+    ),
+    new Country(
+        'c7',
+        'Bułgaria',
+        6808000,
+        110994,
+        'Sofia'
+    ),
+    new Country(
+        'c8',
+        'Chorwacja',
+        4500000,
+        56542,
+        'Zagrzeb'
+    ),
+    new Country(
+        'c9',
+        'Czarnogóra',
+        600000,
+        13812,
+        'Podgorica'
+    ),
+    new Country(
+        'c10',
+        'Czechy',
+        10587000,
+        78866,
+        'Praga'
+    ),
+    new Country(
+        'c11',
+        'Dania',
+        5700000,
+        43098,
+        'Kopenhaga'
+    ),
+    new Country(
+        'c12',
+        'Estonia',
+        1300000,
+        45227,
+        'Talinn'
+    ),
+    new Country(
+        'c13',
+        'Finlandia',
+        5500000,
+        337030,
+        'Helsinki'
+    ),
+    new Country(
+        'c14',
+        'Francja',
+        68300000,
+        551500,
+        'Paryż'
+    ),
+    new Country(
+        'c15',
+        'Grecja',
+        10682000,
+        131990,
+        'Ateny'
+    ),
+    new Country(
+        'c16',
+        'Hiszpania',
+        41420152,
+        504750,
+        'Madryt'
+    ),
+    new Country(
+        'c17',
+        'Holandia',
+        17185000,
+        41526,
+        'Amsterdam'
+    ),
+    new Country(
+        'c18',
+        'Irlandia',
+        4700000,
+        84406,
+        'Dublin'
+    ),
+    new Country(
+        'c19',
+        'Islandia',
+        352444,
+        103125,
+        'Reykjavik'
+    ),
+    new Country(
+        'c20',
+        'Kazachstan',
+        18640000,
+        2724900,
+        'Astana'
+    ),
+    new Country(
+        'c21',
+        'Liechtenstein',
+        39137,
+        160,
+        'Vaduz'
+    ),
+    new Country(
+        'c22',
+        'Litwa',
+        2900000,
+        65300,
+        'Wilno'
+    ),
+    new Country(
+        'c23',
+        'Luksemburg',
+        628000,
+        2586,
+        'Luksemburg'
+    ),
+    new Country(
+        'c24',
+        'Łotwa',
+        2000000,
+        64573,
+        'Ryga'
+    ),
+    new Country(
+        'c25',
+        'Macedonia Północna',
+        2089000,
+        25713,
+        'Skopje'
+    ),
+    new Country(
+        'c26',
+        'Malta',
+        500000,
+        316,
+        'Valetta'
+    ),
+    new Country(
+        'c27',
+        'Mołdawia',
+        3493000,
+        33846,
+        'Kiszyniów'
+    ),
+    new Country(
+        'c28',
+        'Monako',
+        38600,
+        2,
+        'Monako'
+    ),
+    new Country(
+        'c29',
+        'Niemcy',
+        83200000,
+        357578,
+        'Berlin'
+    ),
+    new Country(
+        'c30',
+        'Norwegia',
+        5395000,
+        385207,
+        'Oslo'
+    ),
+    new Country(
+        'c31',
+        'Polska',
+        38000000,
+        322575,
+        'Warszawa'
+    ),
+    new Country(
+        'c32',
+        'Portugalia',
+        10110000,
+        92400,
+        'Lizbona'
+    ),
+    new Country(
+        'c33',
+        'Rosja',
+        147200000,
+        17075200,
+        'Moskwa'
+    ),
+    new Country(
+        'c34',
+        'Rumunia',
+        21000000,
+        238391,
+        'Bukareszt'
+    ),
+    new Country(
+        'c35',
+        'San Marino',
+        29000,
+        61,
+        'San Marino'
+    ),
+    new Country(
+        'c36',
+        'Serbia',
+        6924000,
+        88361,
+        'Belgrad'
+    ),
+    new Country(
+        'c37',
+        'Słowacja',
+        5428000,
+        49034,
+        'Bratysława'
+    ),
+    new Country(
+        'c38',
+        'Słowenia',
+        2000000,
+        20256,
+        'Lublana'
+    ),
+    new Country(
+        'c39',
+        'Szwajcaria',
+        6924000,
+        88361,
+        'Berno'
+    ),
+    new Country(
+        'c40',
+        'Szwecja',
+        10300000,
+        449964,
+        'Sztokholm'
+    ),
+    new Country(
+        'c41',
+        'Turcja',
+        85097000,
+        783562,
+        'Ankara'
+    ),
+    new Country(
+        'c42',
+        'Ukraina',
+        44288000,
+        603700,
+        'Kijów'
+    ),
+    new Country(
+        'c43',
+        'Watykan',
+        3000,
+        0.44,
+        'Watykan'
+    ),
+    new Country(
+        'c44',
+        'Węgry',
+        9604000,
+        93030,
+        'Budapeszt'
+    ),
+    new Country(
+        'c45',
+        'Wielka Brytania',
+        60776238,
+        244820,
+        'Londyn'
+    ),
+    new Country(
+        'c46',
+        'Włochy',
+        60300000,
+        301268,
+        'Rzym'
+    )
+]
---
+++ pam-projekt/models/countries.js
@@ -0,0 +1,10 @@
+class Country{
+    constructor(id, name, population, area, capital){
+        this.id=id
+        this.name=name
+        this.population=population
+        this.area=area
+        this.capital=capital
+    }
+}
+export default Country;
